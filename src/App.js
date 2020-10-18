8af4abc86ca36757dc38700a7c73c308ac66a9fe
Joel H <joel.hacke@gmail.com>
2020-10-18 14:01:59 -0500

    Working With Props

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React from 'react';
 
-function getTitle(title) {
-  return title;
-}
+// same as below but w/ arrows 
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 1,
-    objectID: 1,
-  },
-];
+const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 1,
+      objectID: 1,
+    },
+  ];
 
+  const handlChange = event => {
+    console.log(event.target.value);
+  };
+  return (
+    <div>
+      <h1>My Hacker Stories</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" onChange={handlChange} />
+
+      <hr />
+      <List list={stories} />
+    </div>
+  );
+};
+
+/*
 function App() {
   return (
     <div>
@@ -32,21 +48,25 @@ function App() {
       <input id="search" type="text" />
 
       <hr />
-
-      {list.map(function(item) {
-        return (
-          <div key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </div>
-        );
-      })}
+      <List />
+      <List />
     </div>
   );
 }
+*/
+
+// instead of using props I can deconstruct w/ the {} in the declaration
+// const List = list => and props.map.(item => ) is the other way
+const List = ({list}) =>
+  list.map(item => (
+    <div key={item.objectID}>
+      <span>
+        <a href={item.url}>{item.title}</a>
+      </span>
+      <span>{item.author}</span>
+      <span>{item.num_comments}</span>
+      <span>{item.points}</span>
+    </div>
+  ));
 
 export default App;
